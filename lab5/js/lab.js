324fb8137219cd6d9e3369b9deca8fa8a0b4c30c
bputaski <bkputaski@gmail.com>
2021-10-17 19:33:47 -0500

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Ben Putaski <bputaski@ucsc.edu>
+ * Created: 17 October
+ * License: Public Domain
+ */
+
+ //Define Variables
+ make = "Honda";
+ model = "Odyssey";
+ color = "Red";
+ year = 2015;
+
+ // calculate
+ age = 2021 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
