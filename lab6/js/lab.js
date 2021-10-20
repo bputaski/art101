ab2398f44dd68898fbb21c51191f5d39437cdf78
bputaski <bkputaski@gmail.com>
2021-10-20 17:34:51 -0500

    Update lab.js

---
+++ lab6/js/lab.js
@@ -0,0 +1,26 @@
+/*
+ * Author: Ben Putaski <bputaski@ucsc.edu>
+ * Created: 17 October
+ * License: Public Domain
+ */
+
+
+//define variables
+myTransportArray = ["car", "walking"];
+
+
+var myMainRide = {
+  make = "Honda",
+  model = "Odyssey",
+  color = "Red",
+  year = 2015,
+  age: function() {
+    return 2021 - year;
+  }
+};
+
+
+//output
+document.writeln("Kinds of transportation I use: ", myTransportArray "</br>");
+document.writeln("My main ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
