6c7ea38bb9ac9a677c4f2008d61f28c108a2dfbc
bputaski <bkputaski@gmail.com>
2021-10-26 01:36:04 -0500

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -7,6 +7,9 @@
 // sortUserName - a function that takes user input and sorts the letters of their name
  function sortUserName() {
    var userName = window.prompt("What's your name? I can give you a new one!");
+
+   // convert to lower case
+   userName = userName.toLowerCase();
    console.log("userName =", userName);
 
    //split string to array
