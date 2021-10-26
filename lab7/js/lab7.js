23a73046e14b3c0b19c87c3729ba001354812c70
bputaski <bkputaski@gmail.com>
2021-10-26 01:16:17 -0500

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -3,20 +3,20 @@
  * Created: 25 October
  * License: Public Domain
  */
+
+
  function sortUserName() {
    var userName = window.prompt("What's your name? I can give you a new one!");
-
    console.log("userName =", userName);
-
+   //split string to array
    var nameArray = userName.split('');
    console.log("nameArray =", nameArray);
-
+   //sort the array
    var nameArraySort = nameArray.sort();
    console.log("nameArraySort =", nameArraySort);
-
+   // join array back to a string
    var nameSorted = nameArraySort.join('');
    console.log("nameSorted =", nameSorted);
-
    return nameSorted;
  }
 
