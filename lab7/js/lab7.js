6d98a20defde1ce6abceb0c1c0592e17d979606c
bputaski <bkputaski@gmail.com>
2021-10-26 01:07:26 -0500

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -3,21 +3,22 @@
  * Created: 25 October
  * License: Public Domain
  */
+ // sortUserName - a function that takes user input and sorts the letters of their name
  function sortUserName() {
-     var userName = window.prompt("What's your name? I can give you a new one!");
-
-     console.log("userName =", userName);
-
-     var nameArray = userName.split('');
-     console.log("nameArray =", nameArray);
-
-     var nameArraySort = nameArray.sort();
-     console.log("nameArraySort =", nameArraySort);
-
-     var nameSorted = nameArraySort.join('');
-     console.log("nameSorted =", nameSorted);
-
-     return nameSorted;
+   var userName = window.prompt("Hi, Please tell me your full name so I can fix it.");
+   console.log("userName =", userName);
+   //split string to array
+   var nameArray = userName.split('');
+   console.log("nameArray =", nameArray);
+   //sort the array
+   var nameArraySort = nameArray.sort();
+   console.log("nameArraySort =", nameArraySort);
+   // join array back to a string
+   var nameSorted = nameArraySort.join('');
+   console.log("nameSorted =", nameSorted);
+   return nameSorted;
  }
 
-document.writeln("Here's your brand new name: ". sortUserName(), "<br>");
+
+ // output
+ document.writeln("Oh hey, I've fixed your name: ", sortUserName(), "<br>");
