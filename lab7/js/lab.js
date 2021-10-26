2d435b33f085deae2035b27a3f2338da583968d7
bputaski <bkputaski@gmail.com>
2021-10-25 23:51:44 -0500

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,6 +3,24 @@
  * Created: 25 October
  * License: Public Domain
  */
+ function sortUserName() {
+     var userName = window.prompt("What's your name? I can give you a new one!")
+
+     userName = userName.toLowerCase();
+     console.log("userName =", userName);
+
+     var nameArray = userName.split('');
+     console.log("nameArray =", nameArray);
+
+     var nameArraySort = nameArray.sort();
+     console.log("nameArraySort =", nameArraySort);
+
+     var nameSorted = nameArraySort.join('');
+     console.log("nameSorted =", nameSorted);
+
+     return userName
+ }
+
 
  function randomizeName(userName) {
      // convert userName string to an array
