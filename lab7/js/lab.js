d6132d2a6deafb31cee739536f1833c0c79901b7
bputaski <bkputaski@gmail.com>
2021-10-25 23:28:06 -0500

    yeh

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -4,6 +4,20 @@
  * License: Public Domain
  */
 
+ function randomizeName(userName) {
+     // convert userName string to an array
+     var nameArray = userName.toLowerCase().split("");
+     console.log("nameArray =", nameArray);
+     // shuffle array with our shuffle function
+     var shuffledArray = shuffleArray(nameArray);
+     console.log("shuffledArray =", shuffledArray);
+     var shuffledString = shuffledArray.join("");
+     // shift to Title Case (like a name)
+     var newName = toTitleCase(shuffledString);
+     // return array to a string
+     return newName;
+ }
+
  function main() {
      var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
      document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
