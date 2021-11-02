35ef288669c8528c3bb0a92df3be088139a65947
bputaski <bkputaski@gmail.com>
2021-11-02 08:40:00 -0500

    finishing touches

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,7 +8,7 @@
  var outputEl = document.getElementById("output");
     console.log(outputEl);
 
- //create a new element and assign it to a variable
+ //create new elements and assign it to a variable
   var new1El = document.createElement("p");
     new1El = "This is the first element made in my JavaScript!";
       console.log(new1El);
@@ -17,11 +17,17 @@
     new2El = "This is the second element made in my JavaScript!";
       console.log(new2El);
 
+//change elements to text nodes
   var el1 = document.createTextNode(new1El);
     console.log(el1);
   var el2 = document.createTextNode(new2El);
-  var br = document.createElement("br");
 
+//create empty lines
+  var br = document.createElement("br");
+  var br2 = document.createElement("br");
+
+//append new elements to the 'output' div
   outputEl.appendChild(el1);
   outputEl.appendChild(br);
+  outputEl.appendChild(br2);
   outputEl.appendChild(el2);
