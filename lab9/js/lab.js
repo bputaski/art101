1cb759f0b24a1c984f5d031f97c733e00da5ef52
bputaski <bkputaski@gmail.com>
2021-11-02 08:10:39 -0500

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -6,15 +6,19 @@
 
  //Assign output to a variable
  var outputEl = document.getElementById("output");
+    console.log(outputEl);
 
  //create a new element and assign it to a variable
   var new1El = document.createElement("p");
     new1El = "This is the first element made in my JavaScript!";
+      console.log(new1El);
 
   var new2El = document.createElement("p");
     new2El = "This is the second element made in my JavaScript!";
+      console.log(new2El);
 
   var el1 = document.createTextNode(new1El);
+    console.log(el1);
   var el2 = document.createTextNode(new2El);
 
   outputEl.document.appendChild(new1El);
