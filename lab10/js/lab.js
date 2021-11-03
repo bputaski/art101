49f78613d55f6d84f4896e62467e2003cc5dc47f
bputaski <bkputaski@gmail.com>
2021-11-03 14:32:02 -0500

    Update lab.js

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -5,35 +5,46 @@
  */
 
 
- buttonEl = document.getElementById("my-button");
- console.log("button element:", buttonEl);
- // find the form element
- inputEl = document.getElementById("user-name");
- console.log("input element:", inputEl);
- // find output element
- outputEl = document.getElementById("output");
- console.log("output element:", outputEl);
+ /*
+  * Author: Ben Putaski <bputaski@ucsc.edu>
+  * Created: 3 November
+  * License: Public Domain
+  */
 
 
-  //have the button run the fuction
-  buttonEl.addEventListener('click', function(){
   // sortUserName - a function that takes user input and sorts the letters of their name
-  function sortUserName() {
-    var userName = document.getElementById("user-name").value;
-    console.log("userName =", userName);
+   function sortUserName() {
+     var userName = window.prompt("What's your name? I can give you a new one!");
 
-    //split string to array
-    var nameArray = userName.split('');
-    console.log("nameArray =", nameArray);
+     // convert to lower case
+     userName = userName.toLowerCase();
+     console.log("userName =", userName);
 
-    //sort the array
-    var nameArraySort = nameArray.sort();
-    console.log("nameArraySort =", nameArraySort);
+     //split string to array
+     var nameArray = userName.split('');
+     console.log("nameArray =", nameArray);
 
-    // join array back to a string
-    var nameSorted = nameArraySort.join('');
-    console.log("nameSorted =", nameSorted);
-    return nameSorted;
-  }
-  outputEL.innerHTML = "<p id=name-results>" +  nameSorted + "</p>";
- });
+     //sort the array
+     var nameArraySort = nameArray.sort();
+     console.log("nameArraySort =", nameArraySort);
+
+     // join array back to a string
+     var nameSorted = nameArraySort.join('');
+     console.log("nameSorted =", nameSorted);
+
+     return nameSorted;
+   }
+
+   button = document.getElementById('my-button');
+   inputEl = document.getElementById('user-name');
+   outputEl = document.getElementById('output');
+ 
+   //have the button run the fuction
+   button.addEventListener('click', function(){
+     //get value from name element
+     var userName = inputEL.value;
+     //modify value by running the function
+     var newName = sortUserName(userName);
+     //put new value in output
+     outputEL.innerHTML = "<p id=name-results>" +  newName + "</p>";
+   });
