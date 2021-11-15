95506f62a7a806b86ede9c4be5092fc8130b9055
bputaski <bkputaski@gmail.com>
2021-11-15 14:17:33 -0600

    everything

---
+++ lab12/js/lab.js
@@ -0,0 +1,32 @@
+/*
+ * Author: Ben Putaski <bputaski@ucsc.edu>
+ * Created: 15 November
+ * License: Public Domain
+ */
+
+
+ // Make a button for each section
+ function sortingHatLength(str) {
+   len = str.length;
+   mod = len % 4;
+   if (mod == 0) {
+     return "Gryffindor"
+   }
+   else if (mod == 1) {
+     return "Ravenclaw"
+   }
+   else if (mod == 2) {
+     return "Slytherin"
+   }
+   else if (mod == 3) {
+     return "Hufflepuff"
+   }
+ }
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p>The Sorting Hat has sorted you into the " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+})
