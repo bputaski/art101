a42db9aa9fbf83dcefd278579eeda167da9090b3
bputaski <bkputaski@gmail.com>
2021-11-30 00:05:06 -0600

    Update lab.js

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -12,15 +12,25 @@
 
  function doAPIStuff() {
    console.log("click");
+
+    // Using the core $.ajax() method
    $.ajax({
+     // API endpoint
      url: myUrl,
+     // POST or GET request
      type: "GET",
+     // data type we expect back
      dataType: "json",
    })
+
+// If the request succeeds
+// data is passed back
    .done(function(data){
      console.log("Success:", data);
      $("#output").append("<p>" + data.quote);
    })
+
+// If the request fails
    .fail(function(request,error){
      console.log(request, error);
    })
