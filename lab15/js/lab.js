aaf35d5869d1ae3d23058e834f154a1b17579ea9
bputaski <bkputaski@gmail.com>
2021-11-29 22:52:08 -0600

    Update lab.js

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -10,53 +10,25 @@
   * License: Public Domain
   */
 
-
  var apiEndpoint = "https://api.chucknorris.io/jokes/random";
- var myUrl = "http://numbersapi.com/42/math";
+ var myUrl = "https://api.kanye.rest/";
 
  $("#api-button").click(doAPIStuff);
 
- function putTextOnPage(text){
-   $("#output").html(text);
- }
-
- function doAPIStuff() {
-   console.log("Doing API Stuff");
+ function callAjax() {
+   console.log("click");
    $.ajax({
-     // The URL for the request
-     url: apiEndpoint,
-     // The data to send (will be converted to a query string)
-     data: { },
-     // Whether this is a POST or GET request
+     url: myUrl,
      type: "GET",
-     // The type of data we expect back
-     dataType : "json",
-     // If the request succeeds
-     success: function( data ) {
-       // turn data object into string just so we can display it
-       // and look at it and understand WTF is going on.
-       var textData = JSON.stringify(data);
-       // let's build up a string to display on our page
-       var str = "";
-       // let's get the individual values from data object
-       var imageURL = data.img;
-       var title = data.title;
-       var alt = data.alt;
-       // building our string for display using HTML
-       str += "<h2>" + title + "</h1>";
-       str += "<img src='" + imageURL + "' title='" + alt + "'>"
-       // put our html string on our page using our helper function
-       putTextOnPage(str);
-       // on the console, we can see whaz up
-       console.log("Success:", textData);
-     }
+     dataType: "json",
    })
-   // If the request fails
-   .fail(function( xhr, status, errorThrown ) {
-     console.log("Failure.");
-     putTextOnPage(errorThrown + " Status:" + status);
-     //console.log(errorThrown + " Status:" + status );
+   .done(function(data){
+     console.log("Success:", data);
+     $("#output").append("<p>" + data.quote);
    })
-   console.log("Asynchronously doing the next thing.");
-
+   .fail(function(request,error){
+     console.log(request, error);
+   })
  }
+
+ $("#my-button").click(callAjax);
